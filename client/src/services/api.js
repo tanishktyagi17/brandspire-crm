042a9d6893bd99e26fa16599ed13e365bf2add92
tanishktyagi17 <tanishkt17@gmail.com>
2026-07-23 22:41:23 +0530

    updated api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -5,4 +5,21 @@ const api = axios.create({
   withCredentials: true,
 });
 
+/* ===========================================================
+   Attach JWT Token Automatically
+=========================================================== */
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default api;
